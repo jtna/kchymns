0f18571dbb11c38177b5a5346fc86399927b5b8a
jtna <jtna@X570>
2021-04-15 21:48:00 -0700

    add combo metadata string array

---
+++ make_mdcombo.py
@@ -0,0 +1,36 @@
+import os, re, json
+
+def make_mdcombo():
+    with open('./data/mdcombo.js', 'w') as fout:
+        fout.write('const mdcombo = [\n');
+
+        for i in range(1, 528+1):
+            fname = f'./data/{i:03d}.json'
+
+            with open(fname) as fin:
+                data = json.load(fin)
+                fcomp = ''
+                comps = data['composer']
+                if comps:
+                    comps = comps.replace('\'', '') # e.g. "St. Basil's"
+                    comps = comps.replace('/', '') # e.g. "작사/작곡"
+                    comps = comps.replace('.', '') # e.g. "Albert G. Stein"
+                    comps = comps.split(',')
+
+                    for c in comps:
+                        c = c.removesuffix('작곡')
+                        c = c.removesuffix('편곡')
+                        c = c.removesuffix('선곡')
+                        c = c.removesuffix('작사')
+                        c = c.removesuffix('역사')
+                        c = c.removesuffix('개작')
+                        c = c.strip()
+                        fcomp += c + ' '
+                    fcomp = fcomp.strip()
+
+                fout.write(f"   '{str(i)}, {data['title']}, {fcomp}, {data['firstPhrase']}',\n")
+
+        fout.write('];\n')
+
+if __name__ == '__main__':
+    make_mdcombo()
